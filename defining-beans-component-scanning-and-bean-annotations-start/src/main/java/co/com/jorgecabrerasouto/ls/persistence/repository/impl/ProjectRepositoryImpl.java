83398788ecd0eb737eea08248928c96b0ae8bf4f
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-10-11 10:32:03 -0500

    Module 2. Lesson 3.

---
+++ defining-beans-component-scanning-and-bean-annotations-start/src/main/java/co/com/jorgecabrerasouto/ls/persistence/repository/impl/ProjectRepositoryImpl.java
@@ -0,0 +1,41 @@
+package co.com.jorgecabrerasouto.ls.persistence.repository.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import co.com.jorgecabrerasouto.ls.persistence.model.Project;
+import co.com.jorgecabrerasouto.ls.persistence.repository.IProjectRepository;
+
+@Repository
+public class ProjectRepositoryImpl implements IProjectRepository {
+
+    private List<Project> projects = new ArrayList<>();
+
+    public ProjectRepositoryImpl() {
+        super();
+    }
+
+    @Override
+    public Optional<Project> findById(Long id) {
+        return projects.stream()
+            .filter(p -> p.getId().equals(id))
+            .findFirst();
+    }
+
+    @Override
+    public Project save(Project project) {
+        Project existingProject = findById(project.getId()).orElse(null);
+        if (existingProject == null) {
+            projects.add(project);
+        } else {
+            projects.remove(existingProject);
+            Project newProject = new Project(project);
+            projects.add(newProject);
+        }
+        return project;
+    }
+
+}
