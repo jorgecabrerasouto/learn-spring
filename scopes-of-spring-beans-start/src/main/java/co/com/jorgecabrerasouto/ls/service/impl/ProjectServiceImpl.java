6f65bd74450f3b5841c295f5e841d717e3d785e1
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-10-11 22:50:30 -0500

    Module 2. Lesson 5. 	new file:   scopes-of-spring-beans-start/.classpath 	new file:   scopes-of-spring-beans-start/.project 	new file:   scopes-of-spring-beans-start/.settings/org.eclipse.core.resources.prefs 	new file:   scopes-of-spring-beans-start/.settings/org.eclipse.jdt.apt.core.prefs 	new file:   scopes-of-spring-beans-start/.settings/org.eclipse.jdt.core.prefs 	new file:   scopes-of-spring-beans-start/.settings/org.eclipse.m2e.core.prefs 	new file:   scopes-of-spring-beans-start/.settings/org.springframework.ide.eclipse.prefs 	new file:   scopes-of-spring-beans-start/pom.xml 	new file:   scopes-of-spring-beans-start/src/main/java/co/com/jorgecabrerasouto/ls/LsApp.java 	new file:   scopes-of-spring-beans-start/src/main/java/co/com/jorgecabrerasouto/ls/LsAppConfig.java 	new file:   scopes-of-spring-beans-start/src/main/java/co/com/jorgecabrerasouto/ls/persistence/model/Project.java 	new file:   scopes-of-spring-beans-start/src/main/java/co/com/jorgecabrerasouto/ls/persistence/repository/IProjectRepository.java 	new file:   scopes-of-spring-beans-start/src/main/java/co/com/jorgecabrerasouto/ls/persistence/repository/impl/ProjectRepositoryImpl.java 	new file:   scopes-of-spring-beans-start/src/main/java/co/com/jorgecabrerasouto/ls/service/IProjectService.java 	new file:   scopes-of-spring-beans-start/src/main/java/co/com/jorgecabrerasouto/ls/service/impl/ProjectServiceImpl.java 	new file:   scopes-of-spring-beans-start/src/main/resources/banner.txt 	new file:   scopes-of-spring-beans-start/target/classes/META-INF/MANIFEST.MF 	new file:   scopes-of-spring-beans-start/target/classes/META-INF/maven/com.baeldung/scopes-of-spring-beans-lesson-start/pom.properties 	new file:   scopes-of-spring-beans-start/target/classes/META-INF/maven/com.baeldung/scopes-of-spring-beans-lesson-start/pom.xml 	new file:   scopes-of-spring-beans-start/target/classes/banner.txt 	new file:   scopes-of-spring-beans-start/target/classes/co/com/jorgecabrerasouto/ls/LsApp.class 	new file:   scopes-of-spring-beans-start/target/classes/co/com/jorgecabrerasouto/ls/LsAppConfig.class 	new file:   scopes-of-spring-beans-start/target/classes/co/com/jorgecabrerasouto/ls/persistence/model/Project.class 	new file:   scopes-of-spring-beans-start/target/classes/co/com/jorgecabrerasouto/ls/persistence/repository/IProjectRepository.class 	new file:   scopes-of-spring-beans-start/target/classes/co/com/jorgecabrerasouto/ls/persistence/repository/impl/ProjectRepositoryImpl.class 	new file:   scopes-of-spring-beans-start/target/classes/co/com/jorgecabrerasouto/ls/service/IProjectService.class 	new file:   scopes-of-spring-beans-start/target/classes/co/com/jorgecabrerasouto/ls/service/impl/ProjectServiceImpl.class

---
+++ scopes-of-spring-beans-start/src/main/java/co/com/jorgecabrerasouto/ls/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,28 @@
+package co.com.jorgecabrerasouto.ls.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.com.jorgecabrerasouto.ls.persistence.model.Project;
+import co.com.jorgecabrerasouto.ls.persistence.repository.IProjectRepository;
+import co.com.jorgecabrerasouto.ls.service.IProjectService;
+
+import java.util.Optional;
+
+@Service
+public class ProjectServiceImpl implements IProjectService {
+
+    @Autowired
+    private IProjectRepository projectRepository;
+
+    @Override
+    public Optional<Project> findById(Long id) {
+        return projectRepository.findById(id);
+    }
+
+    @Override
+    public Project save(Project project) {
+        return projectRepository.save(project);
+    }
+
+}
