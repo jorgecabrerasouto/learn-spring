90f3f227e97dad103456a12f74d4994e7cc3bba9
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-10-15 13:12:45 -0500

    Module 3. Lesson 3 End

--- logging-in-a-spring-boot-project-end/src/main/java/co/com/jorgecabrerasouto/ls/service/impl/ProjectServiceImpl.java
+++ logging-in-a-spring-boot-project-end/src/main/java/co/com/jorgecabrerasouto/ls/service/impl/ProjectServiceImpl.java
@@ -2,6 +2,8 @@ package co.com.jorgecabrerasouto.ls.service.impl;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import co.com.jorgecabrerasouto.ls.persistence.model.Project;
@@ -13,17 +15,21 @@ public class ProjectServiceImpl implements IProjectService {
 
     private IProjectRepository projectRepository;
 
+    public static final Logger LOG = LoggerFactory.getLogger(ProjectServiceImpl.class);
+    
     public ProjectServiceImpl(IProjectRepository projectRepository) {
         this.projectRepository = projectRepository;
     }
 
     @Override
     public Optional<Project> findById(Long id) {
+        LOG.debug("Project Service >> Finding Project By Id {}", id);
         return projectRepository.findById(id);
     }
 
     @Override
     public Project save(Project project) {
+        LOG.debug("Project Service >> Saving Project {}", project);
         return projectRepository.save(project);
     }
 
