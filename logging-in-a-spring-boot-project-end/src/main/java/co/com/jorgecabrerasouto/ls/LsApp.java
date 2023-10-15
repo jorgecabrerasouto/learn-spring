ba83c1e0c60b807b60e9ace3353eb042e31eb917
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-10-15 12:43:01 -0500

    Module 3. Lesson 3 Start.

---
+++ logging-in-a-spring-boot-project-end/src/main/java/co/com/jorgecabrerasouto/ls/LsApp.java
@@ -0,0 +1,33 @@
+package co.com.jorgecabrerasouto.ls;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import co.com.jorgecabrerasouto.ls.persistence.model.Project;
+import co.com.jorgecabrerasouto.ls.service.IProjectService;
+
+@SpringBootApplication
+public class LsApp {
+
+    @Autowired
+    private IProjectService projectService;
+
+    public static void main(final String... args) {
+        SpringApplication.run(LsApp.class, args);
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        Project project = new Project("My First Project", LocalDate.now());
+        projectService.save(project);
+
+        Optional<Project> optionalProject = projectService.findById(1L);
+        optionalProject.ifPresent(System.out::println);
+    }
+}
