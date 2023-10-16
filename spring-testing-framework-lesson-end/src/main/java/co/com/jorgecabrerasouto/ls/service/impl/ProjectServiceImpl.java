a1d972aa29521cf1145040139059e6c77355abd6
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-10-16 13:37:10 -0500

    Module 3. Lesson 5 Start.

---
+++ spring-testing-framework-lesson-end/src/main/java/co/com/jorgecabrerasouto/ls/service/impl/ProjectServiceImpl.java
@@ -0,0 +1,43 @@
+package co.com.jorgecabrerasouto.ls.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.com.jorgecabrerasouto.ls.persistence.model.Project;
+import co.com.jorgecabrerasouto.ls.persistence.repository.IProjectRepository;
+import co.com.jorgecabrerasouto.ls.service.IProjectService;
+
+@Service
+public class ProjectServiceImpl implements IProjectService {
+
+    @Autowired
+    private IProjectRepository projectRepository;
+
+    public ProjectServiceImpl() {
+    }
+
+    public ProjectServiceImpl(IProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @Override
+    public Optional<Project> findById(Long id) {
+        return projectRepository.findById(id);
+    }
+
+    @Override
+    public Project save(Project project) {
+        return projectRepository.save(project);
+    }
+
+    public IProjectRepository getProjectRepository() {
+        return projectRepository;
+    }
+
+    public void setProjectRepository(IProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+}
