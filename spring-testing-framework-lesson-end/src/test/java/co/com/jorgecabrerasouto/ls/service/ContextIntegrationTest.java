c19e2e9af8188bd889a76accc47e26128597ea53
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-10-21 16:49:54 -0500

    Module3. Lesson 6 End.

--- spring-testing-framework-lesson-end/src/test/java/co/com/jorgecabrerasouto/ls/service/ContextIntegrationTest.java
+++ spring-testing-framework-lesson-end/src/test/java/co/com/jorgecabrerasouto/ls/service/ContextIntegrationTest.java
@@ -5,7 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
-@SpringJUnitConfig
+@SpringJUnitConfig(value = TestConfig.class)
+public class ProjectServiceIntegrationTest {
+
+    @Autowired
+    private IProjectService projectService;
+
+    @Test
+    public void whenSavingProject_thenOK() {
+        Project savedProject = projectService.save(new Project("name", LocalDate.now()));
+        assertNotNull(savedProject);
+    }
+
+}
 public class ContextIntegrationTest {
 
     @Autowired
