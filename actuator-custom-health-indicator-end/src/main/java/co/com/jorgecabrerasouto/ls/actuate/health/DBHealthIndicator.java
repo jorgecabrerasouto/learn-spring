ca5b00f7f4df11519cac1e588120a08f250d057e
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-10-26 17:48:00 -0500

    Module 4. Lesson3 End.

---
+++ actuator-custom-health-indicator-end/src/main/java/co/com/jorgecabrerasouto/ls/actuate/health/DBHealthIndicator.java
@@ -0,0 +1,25 @@
+package co.com.jorgecabrerasouto.ls.actuate.health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DBHealthIndicator implements HealthIndicator{
+
+    @Override
+    public Health health() {
+        if (isDbUp()) {
+            return Health.up()
+                .build();
+        } else {
+            return Health.down()
+                .withDetail("Error Code", 503)
+                .build();
+        }
+    }
+
+    private boolean isDbUp() {
+        return false;
+    }
+}
