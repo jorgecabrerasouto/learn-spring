b0d6c1d081b4b4022811dc0f8981d25ab3f51148
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-11-03 15:35:40 -0500

    Module 5. Lesson 2 End.

--- beyond-the-default-repository-end/src/test/java/co/com/jorgecabrerasouto/ls/persistence/repository/ProjectRepositoryIntegrationTest.java
+++ beyond-the-default-repository-end/src/test/java/co/com/jorgecabrerasouto/ls/persistence/repository/ProjectRepositoryIntegrationTest.java
@@ -1,10 +1,13 @@
 package co.com.jorgecabrerasouto.ls.persistence.repository;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -12,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import co.com.jorgecabrerasouto.ls.persistence.model.Project;
-import co.com.jorgecabrerasouto.ls.persistence.repository.IProjectRepository;
 
 @SpringBootTest
 public class ProjectRepositoryIntegrationTest {
@@ -35,6 +37,32 @@ public class ProjectRepositoryIntegrationTest {
         Optional<Project> retreivedProject = projectRepository.findById(newProject.getId());
 
         assertEquals(retreivedProject.get(), newProject);
+    }
+
+    @Test
+    public void givenProject_whenFindByName_thenSuccess() {
+        Project newProject = new Project(randomAlphabetic(6), LocalDate.now());
+        projectRepository.save(newProject);
+
+        Optional<Project> retreivedProject = projectRepository.findByName(newProject.getName());
+
+        assertEquals(retreivedProject.get(), newProject);
+    }
+    
+    @Test
+    public void givenProjectCreated_whenFindByDateCreatedBetween_thenSuccess() {
+        Project oldProject = new Project(randomAlphabetic(6), LocalDate.now().minusYears(1));
+        projectRepository.save(oldProject);
+        
+        Project newProject = new Project(randomAlphabetic(6), LocalDate.now());
+        projectRepository.save(newProject);
+        
+        Project newProject2 = new Project(randomAlphabetic(6), LocalDate.now());
+        projectRepository.save(newProject2);
+        
+        List<Project> retrievedProjects = projectRepository.findByDateCreatedBetween(LocalDate.now().minusDays(1), LocalDate.now().plusDays(1));
+        
+        assertThat(retrievedProjects, hasItems(newProject, newProject2));        
     }
 
 }
