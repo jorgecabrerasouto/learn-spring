1dc0dfb69ad95d4907453e059bcfd62458c7f07b
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-11-09 23:43:23 -0500

    Module 6. Lesson 2 End.

--- expanding-our-first-controller-end/src/main/java/co/com/jorgecabrerasouto/ls/web/controller/ProjectController.java
+++ expanding-our-first-controller-end/src/main/java/co/com/jorgecabrerasouto/ls/web/controller/ProjectController.java
@@ -1,20 +1,36 @@
 package co.com.jorgecabrerasouto.ls.web.controller;
 
-import java.time.LocalDate;
-
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import co.com.jorgecabrerasouto.ls.persistence.model.Project;
+import co.com.jorgecabrerasouto.ls.service.IProjectService;
 
 @RestController
 @RequestMapping(value = "/projects")
 public class ProjectController {
+    
+    private IProjectService projectService;
 
-    @GetMapping(path = "/1")
-    public Project findOne() {
-        return new Project("testName", LocalDate.now());
+    public ProjectController(IProjectService projectService) {
+        super();
+        this.projectService = projectService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public Project findOne(@PathVariable Long id) {
+        return projectService.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+    
+    @PostMapping
+    public void create(@RequestBody Project project) {
+        projectService.save(project);
     }
 
 }
