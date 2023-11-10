065f918e1a8eb7fd53353961c1e6a23235a5addd
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2023-11-10 13:33:40 -0500

    Modulo 6. Lesson 4 End.

--- decouple-controller-from-entities-end/src/main/java/co/com/jorgecabrerasouto/ls/web/controller/ProjectController.java
+++ decouple-controller-from-entities-end/src/main/java/co/com/jorgecabrerasouto/ls/web/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package co.com.jorgecabrerasouto.ls.web.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import co.com.jorgecabrerasouto.ls.persistence.model.Project;
 import co.com.jorgecabrerasouto.ls.service.IProjectService;
+import co.com.jorgecabrerasouto.ls.web.dto.ProjectDto;
 
 @RestController
 @RequestMapping(value = "/projects")
@@ -25,14 +27,30 @@ public class ProjectController {
     //
 
     @GetMapping(value = "/{id}")
-    public Project findOne(@PathVariable Long id) {
-        return projectService.findById(id)
+    public ProjectDto findOne(@PathVariable Long id) {
+        Project entity = projectService.findById(id)
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return convertToDto(entity);
     }
 
     @PostMapping
-    public void create(@RequestBody Project newProject) {
-        this.projectService.save(newProject);
+    public void create(@RequestBody ProjectDto newProject) {
+        this.projectService.save(convertToEntity(newProject));
+    }
+    
+    private ProjectDto convertToDto(Project entity) {
+//        return new ProjectDto(entity.getId(), entity.getName(), entity.getDateCreated());
+        return new ProjectDto(entity.getId(), entity.getName());
+    }
+    
+    private Project convertToEntity(ProjectDto dto) {
+        //Project project = new Project(dto.getName(), dto.getDateCreated());
+        Project project = new Project(dto.getName(), null);
+        
+        if(StringUtils.isEmpty((dto.getId()))) {
+            project.setId(dto.getId());
+        }
+        return project;
     }
 
 }
