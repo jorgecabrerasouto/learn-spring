f678320f72ed130b964d7a9e7ae03808d298ad28
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2024-01-12 16:56:56 -0500

    Module 8. Lesson 6. End.

---
+++ intro-to-the-rest-template-end/src/test/java/co/com/jorgecabrerasouto/ls/rest/api/ProjectRestAPILiveTest.java
@@ -0,0 +1,40 @@
+package co.com.jorgecabrerasouto.ls.rest.api;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import java.time.LocalDate;
+import java.util.Random;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import co.com.jorgecabrerasouto.ls.web.dto.ProjectDto;
+
+@SpringBootTest
+public class ProjectRestAPILiveTest {
+
+    private static final String BASE_URL = "http://localhost:8080/projects";
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    public void givenProjectExists_whenGet_thenSuccess() {
+        ResponseEntity<ProjectDto> response = restTemplate.getForEntity(BASE_URL + "/1", ProjectDto.class);
+
+        assertSame(response.getStatusCode(), HttpStatus.OK);
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    public void givenNewProject_whenCreated_thenSuccess() {
+        ProjectDto newProject = new ProjectDto(new Random().nextLong(), "First Project", LocalDate.now());
+        ResponseEntity<Void> response = restTemplate.postForEntity(BASE_URL, newProject, Void.class);
+
+        assertSame(response.getStatusCode(), HttpStatus.OK);
+    }
+
+}
