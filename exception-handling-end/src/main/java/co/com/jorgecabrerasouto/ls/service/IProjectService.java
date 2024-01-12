b316f8adbdda8b82e1d43b93d1c45a9320e243d3
Jorge Cabrera <45431474+jorgecabrerasouto@users.noreply.github.com>
2024-01-12 15:59:29 -0500

    Module 8. Lesson 5. End.

---
+++ exception-handling-end/src/main/java/co/com/jorgecabrerasouto/ls/service/IProjectService.java
@@ -0,0 +1,15 @@
+package co.com.jorgecabrerasouto.ls.service;
+
+import java.util.Optional;
+
+import co.com.jorgecabrerasouto.ls.persistence.model.Project;
+
+public interface IProjectService {
+    Optional<Project> findById(Long id);
+
+    Project save(Project project);
+
+    Iterable<Project> findAll();
+
+    void delete(Long id);
+}
